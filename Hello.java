a9488aea04f461086302bf04a457e8e7cfabac8f
ljy9904 <dlwlsdb99040@gmail.com>
2021-09-24 20:45:42 +0900

    이진유 이름 추가

--- Hello.java
+++ Hello.java
@@ -1,15 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-<<<<<<< HEAD
-
-        System.out.println("ljy");
-
-
-
-=======
         System.out.println("신무환");
->>>>>>> d6517550a5b7c679fda1f1ba296543fb46a0cf28
+        System.out.println("이진유");
+
     }
     
 }
