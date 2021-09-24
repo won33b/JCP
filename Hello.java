b9050f62fd712a2d669abe522b642710b0945912
won33b <won33b@naver.com>
2021-09-24 21:09:30 +0900

    made a conflict

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("신무환");
         System.out.println("이승민");
-        System.out.println("이진유");
+        System.out.println("이원태");
 
     }
     
