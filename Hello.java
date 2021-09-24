c813c293e0eb60cbbee8f686409d59b6001b6dce
LAPTOP-21A4MNL8\k0105 <dltmd127@gmail.com>
2021-09-24 20:42:38 +0900

    이승민 이름 추가

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("신무환");
+        System.out.println("이승민");
     }
     
 }
